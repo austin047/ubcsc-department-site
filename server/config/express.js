1bb0df4b23612a56005d9bd997404292a2b297ae
Fuh Austin <fuhaustin@gmail.com>
2018-09-19 10:56:18 +0100

    removed nodemon --watch from start

--- server/config/express.js
+++ server/config/express.js
@@ -75,7 +75,7 @@ app.get('/kittens', (req,res) => {
 
 
 app.get('/', (req, res) => {
-  res.send('Name is austin');
+  res.send('Name is austin ambe');
   //res.sendFile(path.join(appRoot.path, 'public/dist/index.html'));
 });
 
