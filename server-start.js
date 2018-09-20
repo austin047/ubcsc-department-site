1d908ba11f7fd75558344a2e4d16ca95a3b08e10
Fuh Austin <fuhaustin@gmail.com>
2018-09-20 11:47:52 +0100

    Connect to a mlab database

--- server-start.js
+++ server-start.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV= process.env.NODE_ENV || 'development'
 process.env.SERVER_PORT= process.env.PORT || '3000'
 process.env.JWT_SECRET='0a6b944d-d2fb-46fc-a85e-0295c986cd9f'
-process.env.MONGO_HOST='mongodb://localhost:27017/test2'
+process.env.MONGO_HOST= 'mongodb://admin:admin123@ds062059.mlab.com:62059/tese1' || 'mongodb://localhost:27017/test2' 
 process.env.MONGO_PORT='35574'
 process.env.MAILER_EMAIL_ID='ubcomssa@gmail.com'
 process.env.MAILER_PASSWORD='computerscience2018'
